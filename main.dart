318eddc1a2145ab556094da661e81ebbe4ab80c9
mhsobuz <mahmudsobuz554@gmail.com>
2025-10-01 00:15:17 +0600

    AppBar: Tab Control

--- main.dart
+++ main.dart
@@ -18,39 +18,53 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // Scaffold background color
-      backgroundColor: Colors.white,
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        // Scaffold background color
+        backgroundColor: Colors.white,
 
-      // AppBar Properties
-      // ======================
-      appBar: AppBar(
-        title: Text(
-          "My First App",
-          style: TextStyle(fontWeight: FontWeight.w500),
+        // AppBar Properties
+        // ======================
+        appBar: AppBar(
+          title: Text(
+            "My First App",
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          leading: Icon(Icons.menu),
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.camera),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.notifications),
+            ),
+          ],
+          elevation: 4, // AppBar Shadow
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "Home"),
+              Tab(icon: Icon(Icons.message)),
+              Tab(icon: Icon(Icons.settings)),
+            ],
+          ),
         ),
-        leading: Icon(Icons.menu),
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.white,
-        centerTitle: true,
-      ),
 
-      // Body Properties
-      // =======================
-      body: GridView.builder(
-        padding: EdgeInsets.all(16),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 4,
+        // Body Properties
+        // =======================
+        body: TabBarView(
+          children: [
+            Center(child: Text("Home Screen")),
+            Center(child: Text("Icon Screen")),
+            Center(child: Text("Setting Screen")),
+          ],
         ),
-        itemCount: 28,
-        itemBuilder: (context, index) {
-          return Container(
-            color: Colors.teal,
-            child: Center(child: Text(index.toString())),
-          );
-        },
       ),
     );
   }
