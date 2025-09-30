80a8a5497ea1f9c6d1d2d507f1a871e1ca554e16
mhsobuz <mahmudsobuz554@gmail.com>
2025-09-30 15:50:50 +0600

    Text Styte Change

--- main.dart
+++ main.dart
@@ -20,15 +20,24 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Test App", style: TextStyle(fontWeight: FontWeight.w500)),
+        title: Text(
+          "My First App",
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
         leading: Icon(Icons.menu),
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
+        centerTitle: true,
       ),
       body: Center(
         child: Text(
           "Hello World",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+            backgroundColor: Colors.red,
+          ),
         ),
       ),
     );
